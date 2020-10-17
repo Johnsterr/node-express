6710f181c684f6f0dd5e3be9872cdc8cd9a77343
Evgeniy <evgpashko92@gmail.com>
2020-10-17 18:04:18 +0700

    Add Auth, Sessions and Security

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const csrf = require('csurf');
+const flash = require('connect-flash');
 const mongoose = require('mongoose');
 const Handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const access = require('./access');
 const homeRoutes = require('./routes/home');
@@ -10,7 +14,11 @@ const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cartRoutes = require('./routes/cart');
 const ordersRoutes = require('./routes/orders');
-const User = require('./models/user');
+const authRoutes = require('./routes/auth');
+const varMiddleware = require('./middleware/variables');
+const userMiddleware = require('./middleware/user');
+const url = access;
+
 const app = express();
 
 const hbs = exphbs.create({
@@ -19,46 +27,41 @@ const hbs = exphbs.create({
 	extname: 'hbs'
 });
 
+const store = new MongoStore({
+	collection: 'sessions',
+	uri: url
+})
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
-app.use(async (req, res, next) => {
-	try {
-		const user = await User.findById('5f86ea056879483a6878f4ed');
-		req.user = user;
-		next();
-	} catch (e) {
-		console.log(e);
-	}
-});
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+app.use(session({
+	secret: 'some secret value',
+	resave: false,
+	saveUninitialized: false,
+	store: store
+}));
+app.use(csrf());
+app.use(flash());
+app.use(varMiddleware);
+app.use(userMiddleware);
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/cart', cartRoutes);
 app.use('/orders', ordersRoutes);
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 async function start() {
 	try {
-		const url = access;
 		await mongoose.connect(url, {
 			useNewUrlParser: true,
 			useFindAndModify: false
 		});
-
-		const candidate = await User.findOne();
-
-		if (!candidate) {
-			const user = new User({
-				email: 'evgpashko92@gmail.com',
-				name: 'Evgeniy',
-				cart: {items: []}
-			});
-			await user.save();
-		}
 
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
--- public/app.js
+++ public/app.js
@@ -29,9 +29,13 @@ if ($cart) {
 	$cart.addEventListener('click', event => {
 		if (event.target.classList.contains('js-remove')) {
 			const id = event.target.dataset.id;
+			const csrf = event.target.dataset.csrf;
 
 			fetch('/cart/remove/' + id, {
-				method: 'delete'
+				method: 'delete',
+				headers: {
+					'X-XSRF-TOKEN': csrf
+				}
 			})
 			.then(res => res.json())
 			.then(cart => {
@@ -56,3 +60,5 @@ if ($cart) {
 		}
 	});
 }
+
+M.Tabs.init(document.querySelectorAll('.tabs'));
