4f5bac3367dd192d42a1e471f18b7b4fb5ab1a8a
Evgeniy <evgpashko92@gmail.com>
2020-10-22 17:29:56 +0700

    Add password recovery

---
+++ emails/reset.js
@@ -0,0 +1,18 @@
+const access = require('../access');
+const keys = require('../keys');
+
+module.exports = function(email, token) {
+	return {
+		to: email,
+		from: access.MAIL,
+		subject: 'Восстановление пароля',
+		html: `
+			<h1>Вы забыли пароль?</h1>
+			<p>Если запрос о восстановлении пароля был от вас, то нажмите на ссылку ниже:</p>
+			<p><a href="${keys.BASE_URL}/auth/password/${token}">Восстановить пароль</a></p>
+			<p>Если вы не запрашивали восстановление пароля, то проигнорируйте данное письмо</p>
+			<hr />
+			<a href="${keys.BASE_URL}">Магазин курсов</a>
+		`
+	}
+}
